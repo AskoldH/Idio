b237b749af068e659f87b224953d72569e8407e2
Askold Horčička <askold.horcicka@olc.cz>
2021-08-06 17:18:12 +0200

    Navbar Vue Setting up

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
 
+import Navbar from "./components/navbar/Navbar";
 export default {
   name: 'App',
-
+  components: {Navbar},
 }
 </script>
 
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/js/all'
+
 
 Vue.config.productionTip = false
 
